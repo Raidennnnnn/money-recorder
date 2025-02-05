8f9bf6393a71493ce240e549daae9f44f0979ffd
Delta_Dawn <wuhanawesome@gmail.com>
2025-02-05 14:12:21 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MoneyRecorder from './MoneyRecorder'
+import './App.css'
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <MoneyRecorder />
+    </div>
+  )
+}
+
+export default App
