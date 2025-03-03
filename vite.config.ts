a0247f8589a630e10a452096376834dcc9527039
Delta_Dawn <wuhanawesome@gmail.com>
2025-03-03 13:49:22 +0800

    Integrate Tailwind CSS Vite plugin and update project configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import compression from 'vite-plugin-compression2'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -37,7 +38,7 @@ export default defineConfig({
     },
   }), compression({
     threshold: 500 * 1024,
-  })],
+  }), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
